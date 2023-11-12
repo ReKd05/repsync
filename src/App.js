b23065e0f2259ea677cf0f5680738ae2eb827d9b
DKid <74200348+Mattias3232@users.noreply.github.com>
2023-11-12 01:44:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { IoMdMenu } from 'react-icons/io'; // Importing IoMdMenu icon from Ionicons
+import { AiOutlinePlus } from 'react-icons/ai'; // Importing IoMdMenu icon from Ionicons
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src="./logos/logo.svg" className="App-logo" alt="logo" />
+        <IoMdMenu className="Menu-icon" /> {/* Replaced FontAwesomeIcon with IoMdMenu */}
       </header>
+      <h2 id='welcome-text'>Welcome</h2>
+      <p id='box-header'>Dashboard</p>
+      <div className="dashboard-box">
+        <h2 id='box-message-big'>No saved records.</h2>
+        <p id='box-message-small'>Why not add one?</p>
+      </div>
+      <div className="add-box">
+        <AiOutlinePlus className="add-icon" /> {/* Replacing FontAwesomeIcon with IoMdMenu */}
+      </div>
     </div>
   );
 }
