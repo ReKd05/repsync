bf908642b70811dec23c22e760872fb937717e3e
DKid <74200348+Mattias3232@users.noreply.github.com>
2023-11-12 02:11:17 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { IoMdMenu } from 'react-icons/io'; // Importing IoMdMenu icon from Ionicons
-import { AiOutlinePlus } from 'react-icons/ai'; // Importing IoMdMenu icon from Ionicons
+import { IoMdMenu } from 'react-icons/io';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { BiSolidDashboard } from 'react-icons/bi';
+import { ReactComponent as Logo } from './logos/logo.svg';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-      <img src="./logos/logo.svg" className="App-logo" alt="logo" />
-        <IoMdMenu className="Menu-icon" /> {/* Replaced FontAwesomeIcon with IoMdMenu */}
+        <Logo className="App-logo" />
+        <IoMdMenu className="Menu-icon" onClick={toggleMenu} />
       </header>
-      <h2 id='welcome-text'>Welcome</h2>
-      <p id='box-header'>Dashboard</p>
+      <p id='box-header'><BiSolidDashboard className="icon" /> Dashboard</p>
       <div className="dashboard-box">
         <h2 id='box-message-big'>No saved records.</h2>
         <p id='box-message-small'>Why not add one?</p>
       </div>
       <div className="add-box">
-        <AiOutlinePlus className="add-icon" /> {/* Replacing FontAwesomeIcon with IoMdMenu */}
+        <AiOutlinePlus className="add-icon" />
       </div>
+
+      {isMenuOpen && (
+        <div className="side-menu">
+          <button onClick={toggleMenu}>Close</button>
+          <button>Button 1</button>
+          <button>Button 2</button>
+          <button>Button 3</button>
+        </div>
+      )}
     </div>
   );
 }
